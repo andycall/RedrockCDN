8bfecfba5963565661c28297feddc5de8a7c96b0
Liuchenling <wssgcg1213@qq.com>
2014-09-05 17:07:57 +0800

    add

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var routes = require('./routes/index');
+
 
 var app = express();
 
