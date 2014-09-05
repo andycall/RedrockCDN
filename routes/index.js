aeb0c5a484416e0d6fd48e0309ac88a8130fe62d
Liuchenling <wssgcg1213@qq.com>
2014-09-05 14:16:23 +0800

    add package

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+
+
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+router.get('/cdn', function(req, res) {
+  res.render('index', { title: 'RedRock CDN System' });
 });
+
+router.get('/cdn/js/:jsname', function(req, res) {
+   var js = req.params.jsname.toString().split('@');
+   var jsVersion = js[1] || '';
+   jsname = js[0];
+   console.log(js, jsVersion);
+   fs.readFile('../components/' + jsname + '/' + jsname + '.min.js', function(err, file){
+       res.end(file);
+   })
+
+});
+
+
+
+
+
+
+
+
+
+
+
 
 module.exports = router;
